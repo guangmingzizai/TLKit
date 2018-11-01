57f5a44047d894f7bbcf325b74905e7cb3b44b09
wangjianfei <wangjianfei@yipingfang.com>
2018-11-01 16:39:14 +0800

    fix possible crashs

--- TLKit/TLCategories/Foundation/NSString/NSString+PinYin.m
+++ TLKit/TLCategories/Foundation/NSString/NSString+PinYin.m
@@ -28,10 +28,12 @@
     CFStringTransform((CFMutableStringRef)str, NULL, kCFStringTransformMandarinLatin, NO);
     CFStringTransform((CFMutableStringRef)str, NULL, kCFStringTransformStripDiacritics, NO);
     
-    NSArray *word = [str componentsSeparatedByString:@" "];
-    NSMutableString *initial = [[NSMutableString alloc] initWithCapacity:str.length / 3];
-    for (NSString *str in word) {
-        [initial appendString:[str substringToIndex:1]];
+    NSArray *words = [str componentsSeparatedByString:@" "];
+    NSMutableString *initial = [[NSMutableString alloc] initWithCapacity:words.count];
+    for (NSString *word in words) {
+        if (word.length > 0) {
+            [initial appendString:[word substringToIndex:1]];
+        }
     }
     
     return initial;
